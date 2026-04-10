0f84973a84edd6ef0c0fbf37aa4e40b95cb52a2c
Sushanth Kocharekar <sushantkocharekar271@gmail.com>
2026-04-10 14:32:24 +0530

    vps ip port enabled

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: "http://localhost:5000/api",
+    //baseURL: "http://localhost:5000/api",
     // baseURL: "http://187.127.148.45:5000/api",
-    // baseURL: "http://187.127.148.45:5000/api",  // VPS deployment
+    baseURL: "http://187.127.148.45:5000/api",  // VPS deployment
     // baseURL: "http://192.168.1.12:5000/api",
     baseURL: "https://hchsjobs.space/api",
     withCredentials: true,
